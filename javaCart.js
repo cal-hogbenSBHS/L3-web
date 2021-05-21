bade435ebf9d6a12b38cbf07ccd532c014bd3000
cal-hogbenSBHS <64843626+cal-hogbenSBHS@users.noreply.github.com>
2021-05-21 13:36:55 +1200

    Add files via upload

--- javaCart.js
+++ javaCart.js
@@ -4,12 +4,7 @@ var Items = [
   ['codeImages/skateboardDeck2.jpg', 20.00, 'product 3']
 ];
 
-var cartItems = [
-  ['codeImages/skateboardDeck4.jpg', 29.99, 'product 1'],
-  ['codeImages/SkateDeck.jpg', 15.00, 'product 2'],
-  ['codeImages/skateboardDeck2.jpg', 20.00, 'product 3']
-
-];
+var cartItems = [];
 
 function run() {
   var main = document.getElementById('products');
@@ -31,42 +26,105 @@ function run() {
     ele.appendChild(typebox);
 
     add.dataset.cartIndex = i;
-
     add.addEventListener('click', adding, false);
 
     pic.src = Items[i][0];
     price.innerHTML = '$' + Items[i][1];
     heading.innerHTML = 'product';
-    add.innerHTML = 'click me';
+    add.innerHTML = 'add';
 
     typebox.type = 'number';
     typebox.setAttribute("id", "input" + i);
     typebox.value = 1;
+  }
+} // end func
+
+function adding(Event) {
+  const NUM = Event.currentTarget.dataset.cartIndex;
+
+  cartItems.push([Items[NUM]]);
+  cartItems[Items.length - 1][1] = Number(document.getElementById('input' + NUM).value);
+  updateCart();
+
+} // end func
+
+
+function loadcart() {
+  var main = document.getElementById('cartness');
+
+  var data = sessionStorage.getItem('cartItems');
+  data = JSON.parse(data);
+
+  cartItems = data;
+
+  updateCart();
+
+  for (var i = 0; i < cartItems.length; i++) {
+
+    var ele = document.createElement('li');
+    var pic = document.createElement('img');
+    var heading = document.createElement('h1');
+    var price = document.createElement('h2');
+    var deleteitem = document.createElement('button');
+    var amount = document.createElement('h2');
+    var subtotal = document.createElement('h3');
+
+
+    main.appendChild(ele);
+    ele.appendChild(pic);
+    ele.appendChild(heading);
+    ele.appendChild(price);
+    ele.appendChild(deleteitem);
+    ele.appendChild(amount);
+    ele.appendChild(subtotal);
+
+    deleteitem.dataset.cartIndex = i;
+    deleteitem.addEventListener('click', deleteme, false);
+
+    pic.src = cartItems[i][0][0];
+    price.innerHTML = '$' + cartItems[i][0][1];
+    heading.innerHTML = 'product' + i;
+    deleteitem.innerHTML = 'delete';
+
+    amount.innerHTML = cartItems[i][1];
+    subtotal.innerHTML = '$' + cartItems[i][1] * cartItems[i][0][1];
 
   }
+} // end func
 
-  function adding(Event) {
-    const NUM = Event.currentTarget.dataset.cartIndex;
+function deleteme() {
+  const NUM = event.currentTarget.dataset.cartIndex;
 
-    cartItems.push([Items[NUM]]);
-    cartItems[cartItems.length - 1][1] = Number(document.getElementById('input' + NUM).value);
+  delete cartItems[NUM];
 
-    updateCart();
-  }
-  var totalitems = 0;
+  cartItems = cartItems.filter(item => item !== undefined);
 
-  function updateCart() {
-
-
-    var itemcounter = document.getElementById('itemCount');
-    totalitems = 0;
-    for (var i = 0; i < cartItems.length; i++) {
-      totalitems += cartItems[i][1];
-  itemcounter.innerHTML = totalitems;
-
-
-    }
-
-  }
+  updateCart();
+  loadcart();
+  window.location.reload(true);
 
 }
+
+function adding(Event) {
+  const NUM = Event.currentTarget.dataset.cartIndex;
+
+  cartItems.push([Items[NUM]]);
+  cartItems[cartItems.length - 1][1] = Number(document.getElementById('input' + NUM).value);
+  updateCart();
+
+} // end func
+var totalitems = 0;
+
+function updateCart() {
+  var itemcounter = document.getElementById('checkout');
+
+  totalitems = 0;
+
+  for (var i = 0; i < cartItems.length; i++) {
+    totalitems += cartItems[i][1];
+  }
+  window.sessionStorage.setItem('cartItems', JSON.stringify(cartItems));
+
+  itemcounter.innerHTML = totalitems;
+
+} // end func
